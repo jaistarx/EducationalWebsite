c9816594d41ce98033cf61b783f91c77ed0f885d
nandanasreeraj123 <nandanasreeraj@gmail.com>
2020-10-12 08:12:58 -0700

    2 pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import Lands from './Components/Landing/land';
+import Crs from './Components/Courses/course';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Router} from '@reach/router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <LandingPage path='/'></LandingPage>
+
+        <CoursePage path="/course"></CoursePage>
+      </Router>
     </div>
   );
 }
-
+function LandingPage(){
+  return <Lands></Lands>
+}
+function CoursePage(){
+  return <Crs></Crs>
+}
 export default App;
