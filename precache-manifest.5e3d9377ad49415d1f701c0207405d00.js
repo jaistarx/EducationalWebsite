b03d0e4a5d0ac38493aed4948513f9cfdad10e52
Anu <anamikabav@gmail.com>
2020-10-15 02:35:49 +0530

    Updates

---
+++ precache-manifest.5e3d9377ad49415d1f701c0207405d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7c73e812d10760dbe1e2ac1e1b08dd",
+    "url": "/EducationalWebsite/index.html"
+  },
+  {
+    "revision": "4cbdbb1917fdeb6bdcd2",
+    "url": "/EducationalWebsite/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0a508fa2337fa2d7f271",
+    "url": "/EducationalWebsite/static/css/main.bcd60cb8.chunk.css"
+  },
+  {
+    "revision": "4cbdbb1917fdeb6bdcd2",
+    "url": "/EducationalWebsite/static/js/2.57db3350.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/EducationalWebsite/static/js/2.57db3350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a508fa2337fa2d7f271",
+    "url": "/EducationalWebsite/static/js/main.8e9c5f23.chunk.js"
+  },
+  {
+    "revision": "5776f2e39a3cf579d379",
+    "url": "/EducationalWebsite/static/js/runtime-main.0cb6a613.js"
+  }
+]);
