7db63bd80c3ddb6e146ab907fa723f9a6a439d03
Anu <anamikabav@gmail.com>
2020-10-14 17:53:36 +0530

    frontend created

--- src/Components/Landing/land.js
+++ src/Components/Landing/land.js
@@ -55,8 +55,14 @@ export default function Lands() {
           Some tagline for the educational institution
         </h2>
       </div>
-      <div className="hed">
-        <h4>Our Services</h4>
+
+      {/* Services */}
+      
+      <div className="flexingservice">
+        <img src="images\service.png"></img>
+        <div className="hed">
+          <h4>Our Services</h4>
+        </div>
       </div>
       <div className="flexes">
         <div className="sb">
@@ -93,6 +99,9 @@ export default function Lands() {
           </p>
         </div>
       </div>
+
+      {/* red */}
+
       <div className="reds flexes">
         <div>
           <img
@@ -123,8 +132,14 @@ export default function Lands() {
           <p>Upcoming Events</p>
         </div>
       </div>
+
+      {/* aboutus */}
+      
       <div className="bb">
-        <h4 className="abth">About Us</h4>
+        <div className="flexing">
+          <img src="images\Group.png"></img>
+          <h4 className="abth">About Us</h4>
+        </div>
         <div className="flexes cnt">
           <div>
             <img src="images\undraw_certificate_343v.png" alt="abt"></img>
@@ -141,21 +156,108 @@ export default function Lands() {
         </div>
       </div>
 
+      {/* Downloads */}
+      
       <div className="bb">
-        <h4 className="abth">Our Residential Program</h4>
-        <div className="flexes cnt">
-        <div>
-            <img src="images\undraw_reading_time_gvg0.png" alt="abt"></img>
-            
+        <div className="flexes">
+          <div className="downdiv">
+            <div>
+              <img src="images\undraw_online_test_gba7.png"></img>
+            </div>
+            <div className="downtext">
+              <p>
+                <h4> Download our Engineering app</h4>
+              </p>
+            </div>
+            <div>
+              <button className="downbtn">Click here to Download</button>
+            </div>
           </div>
-          <div className="abttxt">
+          <div className="downdiv">
+            <div>
+              {" "}
+              <img src="images\undraw_learning_2q1h 1.png"></img>
+            </div>
+            <div className="downtext">
+              <p>
+                <h4> Download our Medicine app</h4>
+              </p>
+            </div>{" "}
+            <div>
+              <button className="downbtn">Click here to Download</button>
+            </div>
+          </div>
+          <div className="downdiv">
+            <div>
+              {" "}
+              <img src="images\undraw_book_lover_mkck.png"></img>
+            </div>
+            <div className="downtext">
+              <p>
+                <h4> Download our Brochure now</h4>
+              </p>
+            </div>{" "}
+            <div>
+              <button className="downbtn">Click here to Download</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Residential */}
+      
+      <div className="bb">
+        <div className="flexingres">
+          <img src="images\snowfall 1.png"></img>
+          <h4 className="abth">Our Residential Program</h4>
+        </div>
+        <div className="flexes">
+          <div className="abttxt restxt">
             <p>
               Some random one line about the service.Some random one line about
               the service. Some random one line about the service. Some random
               one line about the service
             </p>
-            
           </div>
+
+          <div>
+            <img src="images\undraw_reading_time_gvg0.png" alt="abt"></img>
+          </div>
+        </div>
+      </div>
+      
+      {/* Testimonials */}
+      
+      <div className="flexingtest">
+        <img src="images\testimonials 1.png"></img>
+        <div className="hed">
+          <h4>Testimonials</h4>
+        </div>
+      </div>
+      <div className="flexes testdiv">
+        <div className="test">
+          <img src="images\download (1) 1.png"></img>
+          <h5 className="mrinalini">Miss Mrinalini</h5>
+          <p>
+            something that miss will have to say about the experience with this
+            organisation
+          </p>
+        </div>
+        <div className="test">
+          <img src="images\download 1.png"></img>
+          <h5 className="mrinalini">Miss Mrinalini</h5>
+          <p>
+            something that miss will have to say about the experience with this
+            organisation
+          </p>
+        </div>
+        <div className="test">
+          <img src="images\teaching-website 1.png"></img>
+          <h5 className="mrinalini">Miss Mrinalini</h5>
+          <p>
+            something that miss will have to say about the experience with this
+            organisation
+          </p>
         </div>
       </div>
     </div>
